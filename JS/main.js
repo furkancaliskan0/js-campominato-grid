d003ed80f6e8cac04203a4aa6e2fcbc6291c850d
furkanGit <furkancaliskan9801@gmail.com>
2022-11-16 22:54:51 +0100

    page completed

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,40 @@
 //Ogni cella ha un numero progressivo, da 1 a 100.
 //Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 //Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// creare contenitore 
+const contenitore = document.getElementById("container");
+
+console.log(contenitore);
+
+// creare bottone
+const button = document.getElementById("button")
+
+button.addEventListener ("click",
+
+    function() {
+        contenitore.innerHTML = '';
+        console.clear();
+        for (let i = 1; i <= 100; i++ ){
+            let myElement = createSquare(i);
+            
+            myElement.addEventListener("click",
+                function() {
+                    myElement.classList.add("clicked");
+                    console.log('Hai cliccato la cella:', i);
+                }
+            )
+            contenitore.appendChild(myElement);
+        }
+    }
+)
+
+function createSquare (i){
+
+    const element = document.createElement("div");
+    element.classList.add("box");
+    element.innerHTML = i;
+
+    return element;
+
+}
